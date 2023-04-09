e6a5cad35bd72b164dd5e039447e65dea915d51d
GbuHti <yzbwcx@gmail.com>
2023-04-09 19:58:41 +0800

    delete dir

--- myqueue/build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yuzhenbo/test/myqueue/googletest/googletest
-# Build directory: /Users/yuzhenbo/test/myqueue/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
